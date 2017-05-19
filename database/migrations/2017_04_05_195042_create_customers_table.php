affe5f89196b34ec11d56666658e7c339181aba8
Liam Walder <liamwalder@Liams-Mac-mini.local>
2017-05-19 17:26:24 +0100

    Correct customer fields.

--- database/migrations/2017_04_05_195042_create_customers_table.php
+++ database/migrations/2017_04_05_195042_create_customers_table.php
@@ -14,9 +14,11 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('surname');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('phone');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
